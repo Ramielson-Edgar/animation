1db4ed7c4384a57ed4e9cba1d8a6ec1d990574ec
Edgar <jurirulez@gmail.com>
2022-04-20 14:27:31 +0300

    add animation

---
+++ js/animation.js
@@ -0,0 +1,215 @@
+const wave = document.querySelector("#wave");
+const circle = document.querySelector("#circle");
+
+const circlePath1 =
+  "M1920.64 0.190063H0.263916V326.151C0.263916 326.151 332.214 877.926 969.032 516.176C1605.85 154.426 1920.64 516.176 1920.64 516.176V0.190063Z";
+
+const shape2 =
+  "M2243.35 0.968018H0.25V322.171C0.25 322.171 510.83 90.2621 1121.8 420.716C1732.77 751.169 2243.35 420.716 2243.35 420.716V0.968018Z";
+const shape3 =
+  "M469.539032,263.986786H-0.000001L0,0c226.11113,0,182.887283-0.414484,469.539032,0V263.986786zz";
+
+let tl = gsap.timeline({
+  defaults: { ease: "power4.inOut", duration: 1 },
+});
+
+gsap.registerPlugin(CSSRulePlugin);
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to("#wave", 0.8, {
+  scrollTrigger: {
+    trigger: "#wave",
+    start: "-500px center",
+  },
+  attr: { d: shape2 },
+  ease: Power2.easeIn,
+});
+
+gsap.to("#wave", 0.8, {
+  scrollTrigger: {
+    trigger: "#wave",
+    start: "-450px center",
+  },
+  attr: { d: shape3 },
+  ease: Power2.easeOut,
+  fill: "#F5F9FF",
+});
+
+gsap.to(circle, 0.8, {
+  scrollTrigger: {
+    trigger: "#circle",
+    start: "top center",
+  },
+  attr: { d: circlePath1 },
+  ease: Power2.easeOut,
+  fill: "#F5F9FF",
+});
+
+// ANIMATION MARKET INSTRUMENTS
+
+// gsap.to(".img", {
+//   scrollTrigger: {
+//     trigger: ".mg",
+//     start: "150px center",
+//     toggleActions: "play pause resume none",
+//   },
+//   stagger: 0.4,
+//   scale: 1,
+//   "clip-path": "circle(50.5% at 50% 50%)",
+//   opacity: 1,
+//   delay: 0.5,
+//   duration: 1.8,
+// });
+
+// SECTION ANIMATION
+gsap.to(".phone-left,.phone-right", {
+  scrollTrigger: {
+    trigger: ".phone-left",
+    start: "-100px center",
+    toggleActions: "play pause resume none",
+  },
+
+  stagger: 0.4,
+  duration: 1,
+  translateY: 0,
+  "clip-path": "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+  delay: 0,
+});
+
+gsap.to(".copy-trading-heading,.copy-trading-desc", {
+  scrollTrigger: {
+    trigger: ".copy-trading-heading",
+    start: "-90px center",
+    toggleActions: "play pause resume none",
+  },
+  stagger: 0.6,
+  opacity: 1,
+  duration: 1.3,
+  y: 0,
+  "clip-path": "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+});
+
+gsap.to(".copy-btn", {
+  scrollTrigger: {
+    trigger: ".copy-trading-heading",
+    start: "-100px center",
+    toggleActions: "play pause resume none",
+  },
+  top: 0,
+  opacity: 1,
+  duration: 1.5,
+});
+
+gsap.to(".research-img", {
+  scrollTrigger: {
+    trigger: ".research-img",
+    start: "-100px center",
+    toggleActions: "play pause resume none",
+  },
+  opacity: 1,
+  y: 0,
+  duration: 1,
+  delay: 0,
+  "clip-path": "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+});
+
+gsap.to(".research-heading,.research-desc", {
+  scrollTrigger: {
+    trigger: ".research-heading",
+    start: "-50px center",
+    toggleActions: "play pause resume none",
+  },
+  stagger: 0.6,
+  opacity: 1,
+  duration: 1.3,
+  y: 0,
+  top: 0,
+  "clip-path": "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+});
+
+gsap.to(".research-btn", {
+  scrollTrigger: {
+    trigger: ".research-btn",
+    start: "-80px center",
+    toggleActions: "play pause resume none",
+  },
+  top: 0,
+  opacity: 1,
+  duration: 1.5,
+});
+
+gsap.to(".support-section-img ", {
+  scrollTrigger: {
+    trigger: ".support-section-img ",
+    start: "-70px center",
+    toggleActions: "play pause resume none",
+  },
+  y: 0,
+  opacity: 1,
+  duration: 1.2,
+  "clip-path": "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+});
+
+gsap.to(CSSRulePlugin.getRule(".support-section .icon-user-plus::after"), {
+  scrollTrigger: {
+    trigger: ".icon-user-plus",
+    start: "120px center",
+    toggleActions: "play pause resume none",
+  },
+
+  duration: 0.8,
+  delay: 0.6,
+  cssRule: {
+    opacity: 1,
+    scale: 1.1,
+  },
+});
+
+gsap.to(".advantage__item", {
+  scrollTrigger: {
+    trigger: ".advantage__item",
+    start: "top center",
+    toggleActions: "play pause resume none",
+  },
+  stagger: 0.4,
+  duration: 1.4,
+  y: 0,
+  opacity: 1,
+});
+
+gsap.to(".advantage__item", {
+  scrollTrigger: {
+    trigger: ".advantage__item",
+    start: "top center",
+    toggleActions: "play pause resume none",
+  },
+  stagger: 0.4,
+  duration: 1.4,
+  y: 0,
+  opacity: 1,
+});
+
+gsap.to(".start-trading-item", {
+  scrollTrigger: {
+    trigger: ".start-trading-item",
+    start: "top center",
+    toggleActions: "play pause resume none",
+  },
+  stagger: 0.4,
+  duration: 1.4,
+  y: 0,
+  opacity: 1,
+});
+
+gsap.to(".start-trading-header", {
+  scrollTrigger: {
+    trigger: ".start-trading-header",
+    start: "-80px center",
+    toggleActions: "play pause resume none",
+  },
+
+  duration: 1,
+  y: 0,
+  opacity: 1,
+});
